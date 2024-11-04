9dbb634f38f650cd5cb8448bf5491d4f7bdc6652
Marcelo A. Cordeiro <marceloalves6@gmail.com>
2024-11-04 23:26:51 +0000

    Add Serilog.

--- src/ExchangeRates.Core/Handlers/GetExchangeRateHandler.cs
+++ src/ExchangeRates.Core/Handlers/GetExchangeRateHandler.cs
@@ -11,8 +11,21 @@ public class GetExchangeRateHandler(IUnitOfWork uow, IMapper mapper) : IRequestH
     {
         var exchage = await uow.ExchangeRepository.GetAsync(request.CurrencyFrom, request.CurrencyTo);
 
+        if (exchage is null)
+        {
+            
+        }
+
         var response = mapper.Map<GetExchangeRateResponse>(exchage);
 
         return response;
     }
 }
+/*
+ Your API Key: ed2a70cbe4f595965df8527b
+ Example Request: https://v6.exchangerate-api.com/v6/ed2a70cbe4f595965df8527b/latest/USD
+*/
+
+/*
+ https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=EUR&apikey=U23W00043X08VE9U
+ */
